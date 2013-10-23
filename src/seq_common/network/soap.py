0c95cc104a5d6ae60f03e2e8091b7fa757005d4c
De Jonckheere Stéphane <humble.jok@gmail.com>
2013-10-23 17:21:30 +0200

    Soap

--- src/seq_common/network/soap.py
+++ src/seq_common/network/soap.py
@@ -1,46 +1,23 @@
 '''
 Created on Oct 22, 2013
 
+FROM: http://stackoverflow.com/questions/6277027/suds-over-https-with-cert
+
 @author: sdejonckheere
 '''
-import urllib2 as u2
-from suds.transport.http import HttpTransport
-import httplib
-import socket
+import requests
+from suds.transport.http import HttpAuthenticated
+from suds.transport import Reply
 
-class HTTPSClientAuthHandler(u2.HTTPSHandler):  
-    def __init__(self, key, cert):  
-        u2.HTTPSHandler.__init__(self)  
-        self.key = key  
-        self.cert = cert  
+class RequestsTransport(HttpAuthenticated):
+    def __init__(self, **kwargs):
+        self.cert = kwargs.pop('cert', None)
+        # super won't work because not using new style class
+        HttpAuthenticated.__init__(self, **kwargs)
 
-    def https_open(self, req):  
-        #Rather than pass in a reference to a connection class, we pass in  
-        # a reference to a function which, for all intents and purposes,  
-        # will behave as a constructor 
-        return self.do_open(self.getConnection, req) 
-
-    def getConnection(self, host, timeout=300):  
-        return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)  
-
-class HTTPSClientCertTransport(HttpTransport):
-    def __init__(self, key, cert, *args, **kwargs):
-        HttpTransport.__init__(self, *args, **kwargs)
-        self.key = key
-        self.cert = cert
-
-    def u2open(self, u2request):
-        """
-        Open a connection.
-        @param u2request: A urllib2 request.
-        @type u2request: urllib2.Requet.
-        @return: The opened file-like urllib2 object.
-        @rtype: fp
-        """
-        tm = self.options.timeout
-        url = u2.build_opener(HTTPSClientAuthHandler(self.key, self.cert))  
-        if self.u2ver() < 2.6:
-            socket.setdefaulttimeout(tm)
-            return url.open(u2request)
-        else:
-            return url.open(u2request, timeout=tm)
+    def send(self, request):
+        self.addcredentials(request)
+        resp = requests.post(request.url, data=request.message,
+                             headers=request.headers, verify=False, cert=self.cert)
+        result = Reply(resp.status_code, resp.headers, resp.content)
+        return result
